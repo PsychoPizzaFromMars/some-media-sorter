f5b5f1952a4ff1bccd874399a7c645b18bfb82b7
PsychoPizzaFromMars <teenagesatanworshipper13@gmail.com>
2021-02-13 17:09:27 +0300

    Update var names, add requirements.txt

--- sorter.py
+++ sorter.py
@@ -5,25 +5,25 @@ import piexif
 import shutil
 import argparse
 
+
 class MediaSortObj:
     def __init__(self, ext):
         self.extension = ext
         self.items_list = []
-    
-    def _date_format(self, date, style):  
+
+    def _date_format(self, date, style):
+        # Output folder name "yyyy-mm"
         if style == 'month':
             if date[5:7] == '00':
                 date = date[:5] + 'undated'
             else:
                 pass
-            # "2019-01"
-
+        # Output folder name "yyyy"
         elif style == 'year':
             if date[5:7] == '00':
                 date = date[:5] + 'undated'
             else:
                 date = date[:4]
-            # "2019"
         return date
 
     def sort(self, files, targetname='Sorted', style='year', outputdir=None):
@@ -42,12 +42,12 @@ class MediaSortObj:
                 else:
                     date_exif = None
 
+                    # Look for DateTime tag among EXIF data
                     for i in ("0th", "Exif", "GPS", "1st"):
                         for tag in pic[i]:
+                            if ((piexif.TAGS[i][tag]["name"] == "DateTime") or ((piexif.TAGS[i][tag]["name"] == "DateTimeOriginal"))):
+                                date_exif = pic[i][tag]
 
-                            if ((piexif.TAGS[i][tag]["name"]=="DateTime") or ((piexif.TAGS[i][tag]["name"]=="DateTimeOriginal"))):
-                                date_exif=pic[i][tag]
-                        
                 if date_exif == None:
                     t = time.localtime(os.path.getmtime(file))
                     date = time.strftime('%Y-%m-%d', t)
@@ -59,28 +59,35 @@ class MediaSortObj:
                     date_exif = date_exif.replace(":", "-")
                     date = date_exif
                     date = self._date_format(date, style)
-                    
-                # Copy files to sorting directories
+
+                # Copy sorted files into output directories
                 if not os.path.exists(date):
                     os.mkdir(date)
                 if not os.path.exists(date + "/" + os.path.basename(file)):
                     shutil.move(file, date + "/" + os.path.basename(file))
 
+
 if __name__ == '__main__':
+    # CLI parsing
     descStr = "This program sorts media files of different extensions by EXIF and system date tags"
     parser = argparse.ArgumentParser(description=descStr)
-
-    parser.add_argument('extensions', help='extensions you want to sort as \".ext1 .ext2 .ext3 ... .extN\"')
     parser.add_argument('inputDir', help='Path to target media files')
-    parser.add_argument('outputDir', help='Path to sorted media files')  
-    parser.add_argument('--dformat', dest='dateFormat', required=False, help='defines sorting dirs - \"year\"(default) or \"month\"')
+    parser.add_argument('outputDir', help='Path to sorted media files')
+    parser.add_argument(
+        '-e', '--extensions', dest='extensions', required=False, help='extensions you want to sort, e.g. \".jpg .png .bmp\" (default=\".jpeg .jpg .png\")')
+    parser.add_argument('--dformat', dest='dateFormat', required=False,
+                        help='defines output folders\' names format - \"year\" (yyyy) or \"month\" (yyyy-mm) (default=\"year\")')
 
     args = parser.parse_args()
 
-    directory = args.inputDir
+    inputdir = args.inputDir
     outputdir = args.outputDir
-    extensions = tuple(args.extensions.split())
-    
+
+    if args.extensions:
+        extensions = tuple(args.extensions.split())
+    else:
+        extensions = ('.jpeg', '.jpg', '.png')
+
     if args.dateFormat:
         datestyle = args.dateFormat
     else:
@@ -88,11 +95,9 @@ if __name__ == '__main__':
 
     target_items = MediaSortObj(extensions)
     media_list = target_items.items_list
-   
-    for root, dirs, files in os.walk(directory):
+
+    for root, dirs, files in os.walk(inputdir):
         for file in files:
             if file.lower().endswith(target_items.extension):
                 media_list.append(os.path.join(root, file))
     target_items.sort(media_list, outputdir=outputdir, style=datestyle)
-
-
